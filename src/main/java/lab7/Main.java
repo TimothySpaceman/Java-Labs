3faa473610bd501a840dd11deff5e9c11699394e
Timothy Rehenel <timothy.rehenel@gmail.com>
2024-12-19 11:36:19 +0200

    lab7: task2

--- src/main/java/lab7/Main.java
+++ src/main/java/lab7/Main.java
@@ -2,7 +2,8 @@ package lab7;
 
 public class Main {
     public static void main(String[] args) {
-        task1();
+//        task1();
+        task2();
     }
 
     public static void task1(){
@@ -29,11 +30,36 @@ public class Main {
             t1.join();
         } catch (InterruptedException e) {
             System.out.println("An error occurred while waiting for join");
+            System.out.println(e.getMessage());
         }
 
         Thread m = Thread.currentThread();
         System.out.println("Main thread name: " + m.getName());
         System.out.println("Main thread priority: " + m.getPriority());
+        System.out.println();
+    }
+
+    public static void task2(){
+        System.out.println("==========TASK 2==========");
+        Thread t1 = new Thread(new MyRunnable());
+        Thread t2 = new Thread(new MyRunnable());
+        Thread t3 = new Thread(new MyRunnable());
+
+        t1.start();
+        t2.start();
+        t3.start();
+
+        try {
+            Thread.sleep(10);
+        } catch (InterruptedException e) {
+            System.out.println("An error occurred while trying to sleep");
+            System.out.println(e.getMessage());
+        }
+
+        t1.interrupt();
+        t2.interrupt();
+        t3.interrupt();
+
         System.out.println();
     }
 }
